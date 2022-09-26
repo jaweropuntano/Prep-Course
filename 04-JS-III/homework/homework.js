84f153c720579bdc33cd1c55d884e46e4f48936e
jaweropuntano <jorgeachavero@hotmail.com>
2022-09-26 18:41:04 -0300

    cambio

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -80,6 +80,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for (var i = 0; i < numeros.length; i++){
+    suma = suma + numeros[i];
+  }
+  return suma;
 }
 
 
